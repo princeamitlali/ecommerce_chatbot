0458afd1dae71f2dcb4b8c1020841fa3b26391b5
Prince Amit <princeamitlali@gmail.com>
2025-06-20 21:18:41 +0530

    Update rag_chain.py

--- rag_chain.py
+++ rag_chain.py
@@ -1,23 +1,3 @@
-# from langchain.chains import RetrievalQA
-# from langchain_community.vectorstores import FAISS
-# from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
-# embedding = OpenAIEmbeddings(api_key=os.getenv("OPENAI_API_KEY"))
-
-
-# vectorstore = FAISS.load_local(
-#     folder_path="vectorstore/schema_knowledge",
-#     embeddings=embedding,
-#     allow_dangerous_deserialization=True
-# )
-
-# retriever = vectorstore.as_retriever()
-# llm = ChatOpenAI(model="gpt-4o", temperature=0, api_key=os.getenv("OPENAI_API_KEY"))
-
-# rag_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
 
 from langchain.chains import RetrievalQA
 from langchain_community.vectorstores import FAISS
@@ -27,28 +7,27 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")  # optional if already in env
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")  
 
-# Use Gemini embeddings
+
 embedding = GoogleGenerativeAIEmbeddings(
-    model="models/text-embedding-004",  # Gemini 1.5 embedding model
+    model="models/text-embedding-004", 
     google_api_key=os.getenv("GOOGLE_API_KEY")
 )
 
-# Load FAISS vector store
+
 vectorstore = FAISS.load_local(
     folder_path="vectorstore/schema_knowledge",
     embeddings=embedding,
     allow_dangerous_deserialization=True
 )
 
-# Create retriever
+
 retriever = vectorstore.as_retriever()
 
-# Use Gemini LLM (chat model)
+
 llm = ChatGoogleGenerativeAI(
     model="gemini-1.5-flash-latest", temperature=0, google_api_key=os.getenv("GOOGLE_API_KEY")
 )
 
-# Build RAG chain
 rag_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
