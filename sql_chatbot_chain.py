18b0bb574610fee1a3ee8a6e9df8cdbd6cdfc442
Prince Amit <princeamitlali@gmail.com>
2025-06-20 21:20:48 +0530

    Update sql_chatbot_chain.py

--- sql_chatbot_chain.py
+++ sql_chatbot_chain.py
@@ -2,7 +2,6 @@ from langchain.agents import AgentExecutor
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from langchain_community.agent_toolkits.sql.base import create_sql_agent
 from langchain_community.utilities import SQLDatabase
-# from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 import re
@@ -11,13 +10,9 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
-# SQLite database
 db = SQLDatabase.from_uri("sqlite:///db/ecommerce.db")
 
-# Language model
-# llm = ChatOpenAI(model="gpt-4o", temperature=0, api_key=os.getenv("OPENAI_API_KEY"))
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash-latest", temperature=0)
-# SQL Agent
 toolkit = SQLDatabaseToolkit(db=db, llm=llm)
 memory = ConversationBufferMemory(return_messages=True)
 
@@ -30,8 +25,6 @@ agent_executor = create_sql_agent(
     memory=memory  # 
 )
 
-
-# Destructive SQL patterns to block
 DESTRUCTIVE_PATTERN = re.compile(r"\b(DELETE|DROP|UPDATE|INSERT|TRUNCATE|ALTER|REPLACE)\b", re.IGNORECASE)
 
 def get_db_schema():
@@ -50,20 +43,16 @@ def db_chain(user_query: str, raw_output=False):
         result = agent_executor.invoke({"input": user_query})
         sql_steps = [step['tool_input'] for step in result.get('intermediate_steps', []) if 'tool_input' in step]
 
-        # Check for destructive SQL like DROP, DELETE, UPDATE, etc.
         for sql in sql_steps:
             print(sql)
             if DESTRUCTIVE_PATTERN.search(sql):
                 return "Query blocked: Destructive statements are not allowed."
 
-        # Final LLM output
         output = result.get("output", "")
 
-        # Return raw structured result if needed
         if raw_output and isinstance(output, list):
             return output
 
-        # Handle "I don't know" and unstructured responses
         if isinstance(output, str) and output.strip().lower() in ["i don't know", "i do not know", "idk"]:
             return "Sorry, I couldn't find an answer to that in the database."
 
